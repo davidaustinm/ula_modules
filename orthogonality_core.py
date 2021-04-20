fb65b3f36411fd68500ea1cd57a503af6eeb87a5
David Austin <david.austin.m@gmail.com>
2021-04-20 19:47:49 -0400

    filenames

---
+++ orthogonality_core.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+
+def projection(b, basis):
+    if len(basis) == 0: return vector([0] * len(b))
+    return sum([ (b*v)/(v*v)*v for v in basis ])
+
+def unit(v):
+    return 1/v.norm() * v
+
+def gs(basis):
+    onbasis = []
+    for b in basis:
+        onbasis.append(unit(b - projection(b, onbasis)))
+    return onbasis
+
+def QR(A):
+    Q = matrix(gs(A.columns())).T
+    return Q, Q.T*A
+
+def onesvec(n):
+    return vector([1] * n)
+
+def demean(v):
+    return v - mean(v)*onesvec(len(v))
+
---
+++ pca_penguins.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def culmen_plot():
+    fig = plt.figure()#figsize=(10,10))
+    ax = fig.add_subplot('111')
+    ax.set_aspect(1)
+    sns.scatterplot(x = 'Culmen Length (mm)', y = 'Culmen Depth (mm)', hue = 'Species', style='Sex', data=df, ax=ax)
+    plt.legend(bbox_to_anchor=(0,1.05), loc=3, borderaxespad=0.)
+    plt.show()
+
+def pca_plot(M, sex='all'):
+    d = np.array([list(c) for c in M.columns()])
+    proj = pd.DataFrame(d, columns=[0,1])
+    proj['Species'] = df['Species']
+    proj['Sex'] = df['Sex']
+    fig = plt.figure()#figsize=(20,10))
+    ax = fig.add_subplot('111')
+    ax.set_aspect(1)
+    if sex == 'all':
+        sns.scatterplot(x = 0, y = 1, hue = 'Species', style='Sex', data=proj, ax=ax)
+    if sex == 'male':
+        proj = proj[proj['Sex'] == 'Male']
+        sns.scatterplot(x = 0, y = 1, hue = 'Species', data=proj, ax=ax)
+    if sex == 'female':
+        proj = proj[proj['Sex'] == 'Female']
+        sns.scatterplot(x = 0, y = 1, hue = 'Species', data=proj, ax=ax)
+    plt.legend(bbox_to_anchor=(-.15,1.05), loc=2, borderaxespad=0.)
+    plt.show()
+
+
+df = pd.read_csv('http://merganser.math.gvsu.edu/david/linear.algebra/ula/data/penguins.csv')
+df = df.drop(columns=['Unnamed: 0'])
+data = pd.read_csv('http://merganser.math.gvsu.edu/david/linear.algebra/ula/data/penguins_data.csv')
+data = data.drop(columns=['Unnamed: 0'])
+A = matrix(data.values).T
