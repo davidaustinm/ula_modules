283783068a0fec4e22cb40529081c407ac718b0c
David Austin <david.austin.m@gmail.com>
2021-06-02 11:00:45 -0400

    iris

--- pca_iris.py
+++ pca_iris.py
@@ -4,8 +4,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 def sepal_plot():
-    fig = plt.figure()#figsize=(10,10))
-    ax = fig.add_subplot('111')
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
     ax.set_aspect(1)
     sns.scatterplot(x = 'sepal length', y = 'sepal width', hue = 'species', style='species', data=df, ax=ax)
     plt.legend(bbox_to_anchor=(0,1.05), loc=3, borderaxespad=0.)
@@ -15,8 +15,8 @@ def pca_plot(M):
     d = np.array([list(c) for c in M.columns()])
     proj = pd.DataFrame(d, columns=[0,1])
     proj['species'] = df['species']
-    fig = plt.figure()#figsize=(20,10))
-    ax = fig.add_subplot('111')
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
     ax.set_aspect(1)
     sns.scatterplot(x = 0, y = 1, hue = 'species', style='species', data=proj, ax=ax)
     plt.legend(bbox_to_anchor=(0,1.05), loc=3, borderaxespad=0.)
