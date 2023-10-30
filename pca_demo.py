e2e51bc15a1e3173eb93c8561bee5ae0ec8eef46
David Austin <david.austin.m@gmail.com>
2023-10-30 09:36:30 -0400

    pca

--- pca_demo.py
+++ pca_demo.py
@@ -8,6 +8,8 @@ def pca_plot(m):
 df = pd.read_csv('https://raw.githubusercontent.com/davidaustinm/ula_modules/master/data/pca-demo.csv', index_col=0)	      
 A = matrix([vector(row) for row in df.values]).T
 
-fig = plt.figure().gca(projection='3d')
-fig.scatter(df['0'], df['1'], df['2'])                                
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+
+ax.scatter(df['0'], df['1'], df['2'])                                
 plt.show()
